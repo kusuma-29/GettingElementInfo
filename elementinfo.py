53bf4a92dfa2f6c857443cbef3036c2eb92202f5
kusuma-29 <142474215+kusuma-29@users.noreply.github.com>
2024-08-02 09:57:42 +0530

    Update elementinfo.py

--- elementinfo.py
+++ elementinfo.py
@@ -1,15 +1,11 @@
 import periodictable
-def elementinfo(symbol):
-    if not periodictable.elements.symbol(symbol):
-        print("Invalid element symbol.")
-        return
-
-    element = periodictable.elements.symbol(symbol)
+element_symbol = input("Enter the symbol of the element: ")
+if not periodictable.elements.symbol(element_symbol):
+    print("Invalid element symbol.")
+else:
+    element = periodictable.elements.symbol(element_symbol)
     print(f"Element: {element.name}")
     print(f"Symbol: {element.symbol}")
     print(f"Atomic Number: {element.number}")
     print(f"Atomic Weight: {element.mass}")
     print(f"Density: {element.density}")
-element_symbol = input("Enter the symbol of the element: ")
-elementinfo(element_symbol)
-
